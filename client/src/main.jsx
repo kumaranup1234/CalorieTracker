acc563bebe6ad526ece7bcae7cdaa7d11bbf6209
Anup Kumar <anupg2551@gmail.com>
2026-02-04 00:47:24 +0530

    Added backend url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
+
+// Set production API URL from Environment Variable
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
